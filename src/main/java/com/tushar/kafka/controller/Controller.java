b33e897be7c325742a04babfb22d1347d3db7068
tushar mahat <tusharmahat@gmail.com>
2024-02-28 21:34:43 -0700

    initial commit

---
+++ src/main/java/com/tushar/kafka/controller/Controller.java
@@ -0,0 +1,25 @@
+package com.tushar.kafka.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tushar.kafka.producer.KafkaProducer;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class Controller {
+	@Autowired
+	private KafkaProducer kafkaProducer;
+
+	@GetMapping("/publish")
+	public ResponseEntity<String> publish(@RequestParam("message") String message){
+		kafkaProducer.sendMessage(message);
+		return new ResponseEntity<String>("Message sent to topic",HttpStatus.OK);
+	}
+
+}
---
+++ src/test/java/com/tushar/kafka/KafkaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tushar.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
